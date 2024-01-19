af57a9429aee64a00531053cd43a6b8e97dee81a
João José <joaojose@Joaos-MacBook-Pro.local>
2024-01-19 20:57:52 -0300

    feature: get_max_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -7,7 +7,15 @@ class ProcessSalaries(ProcessJobs):
         super().__init__()
 
     def get_max_salary(self) -> int:
-        pass
+        max_salary = 0
+
+        for job in self.jobs_list:
+            salary_range = job.get("max_salary")
+            if salary_range and salary_range.isdigit():
+                salary = int(salary_range)
+                max_salary = max(max_salary, salary)
+
+        return max_salary
 
     def get_min_salary(self) -> int:
         pass
