d5add6923c169580e743751ca90510c3c56e1aaf
João José <joaojose@Joaos-MacBook-Pro.local>
2024-01-19 17:31:05 -0300

    feature: get unique_job_types. req. 3

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -12,8 +12,17 @@ class ProcessJobs:
             self.job_list = [dict(row) for row in csv_reader]
 
         return self.job_list
+   
     def get_unique_job_types(self) -> List[str]:
-        pass
+        unique_job_types = set()  # Usamos um conjunto para garantir valores únicos
+
+        for job in self.jobs_list:
+            job_type = job.get('job_type', '')
+            if job_type:
+                unique_job_types.add(job_type)
+
+        return list(unique_job_types)
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
+    
