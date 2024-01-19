71a1b7a3314977a1c081b01c2b41154b20339895
João José <joaojose@Joaos-MacBook-Pro.local>
2024-01-19 18:35:26 -0300

    fixing linter

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List
 import csv
 
 
@@ -7,18 +7,18 @@ class ProcessJobs:
         self.job_list = list()
 
     def read(self, path):
-        with open(path, 'r', newline='') as file:
+        with open(path, "r", newline="") as file:
             csv_reader = csv.DictReader(file)
             self.job_list = [dict(row) for row in csv_reader]
 
         return self.job_list
-   
+
     def get_unique_job_types(self) -> List[str]:
-        unique_job_types = set()  
-       
+        unique_job_types = set()
+
         for job in self.job_list:
-            job_type = job.get('job_type', '')
-            
+            job_type = job.get("job_type", "")
+
             if job_type:
                 unique_job_types.add(job_type)
         print(unique_job_types)
@@ -26,7 +26,8 @@ class ProcessJobs:
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
-    
+
+
 # process = ProcessJobs()
 # process.read("data/jobs.csv")
 # result = process.get_unique_job_types()
