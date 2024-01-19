530d55ae010bdde43ec230cd7f6bb904abcb85ff
João José <joaojose@Joaos-MacBook-Pro.local>
2024-01-19 20:46:13 -0300

    feature: get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,10 +1,17 @@
 from src.insights.jobs import ProcessJobs
 from typing import List
 
-
 class ProcessIndustries(ProcessJobs):
     def __init__(self):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        unique_industries = set()
+
+        for job in self.jobs_list:
+            industry = job.get('industry', '')
+            if industry:
+                unique_industries.add(industry)
+
+        return list(unique_industries)
+
