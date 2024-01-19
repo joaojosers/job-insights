a348f8a114c3514943062c1f8b07733ff6d25d15
João José <joaojose@Joaos-MacBook-Pro.local>
2024-01-19 19:11:54 -0300

    fixing linter test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,7 +2,6 @@ from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    # path = "/Users/joaojose/Documents/trybe-projects-cs/sd-032-a-project-job-insights/data/jobs.csv"
     path = "data/jobs.csv"
     word = "nurse"
     assert count_ocurrences(path, word) == 276
