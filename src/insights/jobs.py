929859db486afc1cf7a63631ec8c19ee2d95129a
João José <joaojose@Joaos-MacBook-Pro.local>
2024-01-19 16:56:31 -0300

    feature: read csv. req. 2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,13 +1,17 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path):
+        with open(path, 'r', newline='') as file:
+            csv_reader = csv.DictReader(file)
+            self.job_list = [dict(row) for row in csv_reader]
 
+        return self.job_list
     def get_unique_job_types(self) -> List[str]:
         pass
 
