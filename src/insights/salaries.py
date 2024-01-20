744a207bd7e3cb19ec8612e0d25a2693c7e37e25
João José <joaojose@Joaos-MacBook-Pro.local>
2024-01-19 21:42:43 -0300

    feature: matches_salary_range

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -30,8 +30,48 @@ class ProcessSalaries(ProcessJobs):
         # Retorna 0 se não houver salários válidos
         return min_salary if min_salary != float("inf") else 0
 
-    def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
-        pass
+    # def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
+    #     pass
+
+    def matches_salary_range(self, job: Dict, salary: str) -> bool:
+        # Verifica se as chaves min_salary e max_salary estão presentes no dicionário
+        if (
+            "min_salary" not in job
+            or job["min_salary"] is None
+            or "max_salary" not in job
+            or job["max_salary"] is None
+        ):
+            raise ValueError(
+                "As chaves min_salary e max_salary são obrigatórias no dicionário."
+            )
+
+        # Converte os valores de min_salary e max_salary para números
+        try:
+            print(job["min_salary"])
+            min_salary = float(job["min_salary"])
+            max_salary = float(job["max_salary"])
+        except ValueError:
+            raise ValueError(
+                "As chaves min_salary e max_salary devem ter valores numéricos."
+            )
+        except TypeError:
+            raise ValueError
+        # Verifica se min_salary é maior que max_salary
+        if min_salary > max_salary:
+            raise ValueError(
+                "O valor de min_salary não pode ser maior que max_salary."
+            )
+
+        # Converte o valor de salary para número
+        try:
+            salary = float(salary)
+        except TypeError:
+            raise ValueError
+        except ValueError:
+            raise ValueError("O parâmetro salary deve ter valor numérico.")
+
+        # Retorna True se o salário estiver dentro da faixa salarial, caso contrário, retorna False
+        return min_salary <= salary <= max_salary
 
     def filter_by_salary_range(
         self, jobs: List[dict], salary: Union[str, int]
